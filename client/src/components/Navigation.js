d985c17cd577e045cc432f9b667cc9e042945658
Ricardo Barbosa <ric3639@gmail.com>
2020-02-01 21:12:26 -0800

    App.js and suggested styles added

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Name,
+  Logo,
+  Header,
+  HeaderLeft,
+  Metadata,
+  DashboardLink,
+  LogoutButton
+} from "../styled/NavigationStyles.js";
+import { useHistory } from "react-router-dom";
+
+const Navigation = ({ user, setLoggedIn }) => {
+  const history = useHistory();
+  const handleClick = () => {
+    window.localStorage.clear();
+    setLoggedIn(false);
+    history.push("/login");
+  };
+  return (
+    <Header>
+      <HeaderLeft>
+        <Logo>
+          <span>Pro</span>
+          <span>Lift</span>
+        </Logo>
+        <DashboardLink to="/dashboard">Dashboard</DashboardLink>
+      </HeaderLeft>
+      <Metadata>
+        <p>
+          Logged in as:
+          <br />
+          <Name>
+            <span>{user.firstName}</span>
+            <span>{user.lastName}</span>
+          </Name>
+        </p>
+        <LogoutButton onClick={() => handleClick()}>Log Out</LogoutButton>
+      </Metadata>
+    </Header>
+  );
+};
+
+export default Navigation;
